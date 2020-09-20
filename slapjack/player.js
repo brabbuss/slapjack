fbd008a7e802b86e2ef93a7d275f16f315481005
Scott Brabson <scottbrabson@gmail.com>
2020-09-19 20:28:19 -0600

    Add change to where cards are checked for valid win combinations

--- slapjack/player.js
+++ slapjack/player.js
@@ -8,32 +8,42 @@ class Player {
     this.winner = false;        // just in case
     this.suddenDeathLeader = false
   }
+  switchTurns() {
+    this.myTurn = !this.otherPlayer.myTurn
+  }
+  keepTurn() {
+    this.myTurn = true
+    this.otherPlayer.myTurn = false
+  }
+  loseTurn() {
+    this.myTurn = false
+    this.otherPlayer.myTurn = true
+  }
   dealCard(game) {
     if (game.suddenDeathMode !== true && this.myTurn === true) {
       game.discardPile.unshift(this.myDeck[0]);       // place on discard pile array
       this.myDeck.shift();      // pull card from my card
-      this.myTurn = false;
-      this.otherPlayer.myTurn = true;
+      this.loseTurn();
+    } else if (this.suddenDeathLeader === true) {
+      game.discardPile.unshift(this.myDeck[0]);
+      this.myDeck.shift();
     } else {
       console.log(`not your turn ${this.player}`);
-      if (this.suddenDeathLeader === true) {
-        game.discardPile.unshift(this.myDeck[0]);
-        this.myDeck.shift();
-      }
     }
   }
-  slapCard(player, game) {
+    slapCard(player, game) {
     if (this.slapValidation(game) === true && this.suddenDeathLeader === true) {
       this.collectDiscardPile(game)
       player.winner = true;
       game.gameOver = true;
       //if you successfully slap as leader. you win
-    } else if (this.slapValidation(game) === false && this.suddenDeathLeader === false) {
+    } else if (this.slapValidation(game) === false && this.suddenDeathLeader === false && game.suddenDeathMode === true) {
+      alert("game over")
       game.gameOver = true;
     } else if (this.slapValidation(game) === true) {  // legal slap
-      this.collectDiscardPile(game)
+      this.collectDiscardPile(game);
       return true
-    } else {
+    } else if (this.slapValidation(game) === false) {
       if (game.discardPile === undefined) {
         alert("bad slap - no cards to give")
         // TODO slaps w/empty - might be way to skip suddendeath mode COMPLEXITIES!???
@@ -41,7 +51,6 @@ class Player {
       } else {
         this.otherPlayer.myDeck.unshift(this.myDeck[0]);  //illegal slap
         this.myDeck.shift()
-        this.myTurn = false;
         return false
       }
     }
@@ -49,19 +58,23 @@ class Player {
   slapValidation(game) {
     if (validBasicSlaps.indexOf(game.discardPile[0]) !== -1) {  //  if the string at [0] is not included in basic slaps array, = -1 (void)
       console.log("trump card");
+      this.keepTurn()
       return true;
-    } else if (game.discardPile.length === 2) {
+    } else if (game.discardPile.length < 3) {
       if (game.discardPile[0].charAt(3) === game.discardPile[1].charAt(3)) {  //  leveraging naming convention to match
         console.log("doubles");
+        this.keepTurn()
         return true;
       }
     } else if (game.discardPile.length > 2) {
       if (game.discardPile[0].charAt(3) === game.discardPile[2].charAt(3) || game.discardPile[0].charAt(3) === game.discardPile[1].charAt(3)) {
         console.log("sandwich");
+        this.keepTurn()
         return true;
       }
     } else {
       console.log("bad slap");
+      this.loseTurn();
       return false;
     }
   }
@@ -69,7 +82,5 @@ class Player {
     this.myDeck = this.myDeck.concat(game.discardPile);
     game.discardPile = [];
     game.shuffleDeck(this.myDeck);
-    this.myTurn = true
-    this.otherPlayer.myTurn = false;
   }
 }
